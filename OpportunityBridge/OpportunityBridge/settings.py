fc41c1b8d4e48142f7e1eacb686cf382262ff903
Yaminibhekare <yaminibhekare30@gmail.com>
2023-03-03 00:36:43 +0530

    Yamini commit

--- OpportunityBridge/OpportunityBridge/settings.py
+++ OpportunityBridge/OpportunityBridge/settings.py
@@ -125,3 +125,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 STATICFILES_DIRS =[
     BASE_DIR,"static"
 ]
+
--- OpportunityBridge/OpportunityBridge/views.py
+++ OpportunityBridge/OpportunityBridge/views.py
@@ -5,3 +5,8 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate,login,logout
+
+def login(request):
+    return render(request, "login.html")
+def register(request):
+    return render(request, "register.html")
